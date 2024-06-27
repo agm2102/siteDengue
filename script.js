48e33e38996902c5e206b22c36af5ecc60e6cda8
agm2102 <97256008+agm2102@users.noreply.github.com>
2024-06-26 23:15:40 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,83 +1,85 @@
+
 let sintomas = document.querySelector("#sintomas")
 let proliferacao = document.querySelector("#proliferacao")
 let diagnostico = document.querySelector("#diagnostico")
 let prevencao = document.querySelector("#prevencao")
 let tratamento = document.querySelector("#tratamento")
-let btnInicio = document.querySelector(".btnInicio")
+let oqueEdengue = document.querySelector("#oqueEdengue")
 
+let sessaoOqueEdengue = document.getElementById("sessaoOqueEdengue")
+let sessaoSintomas = document.getElementById("sessaoSintomas")
+let sessaoProliferacao = document.getElementById("sessaoProliferacao")
+let sessaoDiagnostico = document.getElementById("sessaoDiagnostico")
+let sessaoPrevencao = document.getElementById("sessaoPrevencao")
+let sessaoTratamento = document.getElementById("sessaoTratamento")
+
+let btnInicio = document.querySelector(".btnInicio")
 const alturaTela = window.screen.height
 
 sintomas.addEventListener('click', () => {
 
-    switch (alturaTela) {
-        case 1440:
-            window.scroll({
-                top: ((alturaTela * 68.5) / 100),
-                behavior: "smooth"
-            })
-            break;
-        case 1080:
-            window.scroll({
-                top: ((alturaTela * 96) / 100),
-                behavior: "smooth"
-            })
-            break
-        case 720:
-            window.scroll({
-                top: ((alturaTela * 149) / 100),
-                behavior: "smooth"
-            })
-            break
-    }
+    sessaoPrevencao.style.transform = 'translateX(200%)'
+    sessaoTratamento.style.transform = 'translateX(200%)'
+    sessaoProliferacao.style.transform = 'translateX(200%)'
+    sessaoDiagnostico.style.transform = 'translateX(200%)'
+    sessaoOqueEdengue.style.transform = 'translateX(200%)'
+    sessaoSintomas.style.transform = 'translateX(0%)'
 })
 diagnostico.addEventListener('click', () => {
-    switch (alturaTela) {
-        case 1440:
-            window.scroll({
-                top: ((alturaTela * 128) / 100),
-                behavior: "smooth"
-            })
-            break;
-        case 1080:
-            window.scroll({
-                top: ((alturaTela * 112) / 100),
-                behavior: "smooth"
-            })
-            break
-        case 720:
-            window.scroll({
-                top: ((alturaTela * 176) / 100),
-                behavior: "smooth"
-            })
-            break
-    }
+
+    sessaoPrevencao.style.transform = 'translateX(200%)'
+    sessaoTratamento.style.transform = 'translateX(200%)'
+    sessaoProliferacao.style.transform = 'translateX(200%)'
+    sessaoDiagnostico.style.transform = 'translateX(0%)'
+    sessaoOqueEdengue.style.transform = 'translateX(200%)'
+    sessaoSintomas.style.transform = 'translateX(200%)'
+
 })
 prevencao.addEventListener('click', () => {
-    switch (alturaTela) {
-        case 1440:
-            window.scroll({
-                top: ((alturaTela * 139) / 100),
-                behavior: "smooth"
-            })
-            break;
-        case 1080:
-            window.scroll({
-                top: ((alturaTela * 187) / 100),
-                behavior: "smooth"
-            })
-            break
-        case 720:
-            window.scroll({
-                top: ((alturaTela * 291) / 100),
-                behavior: "smooth"
-            })
-            break
-    }   
-})
+    sessaoPrevencao.style.transform = 'translateX(0%)'
+    sessaoTratamento.style.transform = 'translateX(200%)'
+    sessaoProliferacao.style.transform = 'translateX(200%)'
+    sessaoDiagnostico.style.transform = 'translateX(200%)'
+    sessaoOqueEdengue.style.transform = 'translateX(200%)'
+    sessaoSintomas.style.transform = 'translateX(200%)'
 
-btnInicio.addEventListener('click', () => {
-    window.scroll({
-        top: 0,
-        behavior: "smooth"
-    })
+})
+tratamento.addEventListener('click', () => {
+    sessaoPrevencao.style.transform = 'translateX(200%)'
+    sessaoTratamento.style.transform = 'translateX(0%)'
+    sessaoPrevencao.style.transform = 'translateX(200%)'
+    sessaoDiagnostico.style.transform = 'translateX(200%)'
+    sessaoOqueEdengue.style.transform = 'translateX(200%)'
+    sessaoSintomas.style.transform = 'translateX(200%)'
+
+})
+proliferacao.addEventListener('click', () => {
+    sessaoPrevencao.style.transform = 'translateX(200%)'
+    sessaoTratamento.style.transform = 'translateX(200%)'
+    sessaoProliferacao.style.transform = 'translateX(0%)'
+    sessaoDiagnostico.style.transform = 'translateX(200%)'
+    sessaoOqueEdengue.style.transform = 'translateX(200%)'
+    sessaoSintomas.style.transform = 'translateX(200%)'
+
+})
+oqueEdengue.addEventListener('click', () => {
+    sessaoPrevencao.style.transform = 'translateX(200%)'
+    sessaoTratamento.style.transform = 'translateX(200%)'
+    sessaoProliferacao.style.transform = 'translateX(200%)'
+    sessaoDiagnostico.style.transform = 'translateX(200%)'
+    sessaoOqueEdengue.style.transform = 'translateX(0%)'
+    sessaoSintomas.style.transform = 'translateX(200%)'
+
+})
+document.addEventListener('keydown', (eve)=>{
+    const key = eve.key
+    if(key == 'Tab'){
+        sessaoPrevencao.style.transform = 'translateX(200%)'
+        sessaoTratamento.style.transform = 'translateX(200%)'
+        sessaoProliferacao.style.transform = 'translateX(200%)'
+        sessaoDiagnostico.style.transform = 'translateX(200%)'
+        sessaoOqueEdengue.style.transform = 'translateX(200%)'
+        sessaoSintomas.style.transform = 'translateX(0%)'
+        window.location.reload()
+    }
 })
